543dfd7fb1853f532051b889e2854bda2b0942d4
Owolabi Pius <opius2007@gmail.com>
2023-05-31 13:21:36 +0100

    binary tree update

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,4 @@
-#ifndef BINARY_TREES_H
+.#ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
 #include <stddef.h>
@@ -81,5 +81,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+avl_t *array_to_avl(int *array, size_t size)
 
 #endif /* BINARY_TREES_H */
