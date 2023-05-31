29cccf5f147957873d4037c83dcb0b8ce0a0be8e
Owolabi Pius <opius2007@gmail.com>
2023-05-31 13:34:48 +0100

    binary tree update

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -73,23 +73,3 @@ avl_t *avl_insert(avl_t **tree, int value)
 	r_insert_node(tree, *tree, &new, value);
 	return (new);
 }
-
-/**
- * array_to_avl - Builds an AVL tree from an array
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
- */
-avl_t *array_to_avl(int *array, size_t size)
-{
-	avl_t *root = NULL;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		avl_insert(&root, array[i]);
-
-	return (root);
-}
--- binary_trees.h
+++ binary_trees.h
@@ -81,6 +81,5 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-avl_t *array_to_avl(int *array, size_t size)
 
 #endif /* BINARY_TREES_H */
